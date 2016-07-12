7e0417b64a39e3e4533405fc763df44c8f7970c4
tmullen <tony.mullen@gmail.com>
2016-07-13 08:58:05 +0900

    opened add event to unauthenticated users

--- server/policies/cal-events.server.policy.js
+++ server/policies/cal-events.server.policy.js
@@ -34,10 +34,10 @@ exports.invokeRolesPolicies = function () {
     roles: ['guest'],
     allows: [{
       resources: '/api/calendar',
-      permissions: ['get']
+      permissions: ['get', 'post']
     }, {
       resources: '/api/calendar/:calEventId',
-      permissions: ['get']
+      permissions: ['*']
     }]
   }]);
 };
